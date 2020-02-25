a694940e1749a638121367bf40694c7f03a5c22f
rad710 <aradonci@stevens.edu>
2020-02-25 17:46:48 -0500

    Created and set up the team's unit testing python file where all unit testing will be done

---
+++ UnitTesting.py
@@ -0,0 +1,9 @@
+import Project03
+import unittest
+
+class TestUserStories(unittest.TestCase):
+    def test_unittest(self): #this is a test unit test
+        self.assertEqual('test'.upper(), 'TEST')
+
+if __name__ == '__main__':
+    unittest.main()
